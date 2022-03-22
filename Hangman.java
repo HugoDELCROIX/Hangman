5a8a87220629de7d4bce66d842681c431007daa9
HugoDELCROIX <hugo.delcroix@hotmail.com>
2022-03-22 22:40:21 +0100

    verify method added

--- Hangman.java
+++ Hangman.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class Hangman {
     protected static String word = "tennis";
     protected boolean win = false;
+    Scanner s = new Scanner(System.in);
 
-    public static String censure(String censoredWord){
-        censoredWord = "";
+    public static String censure(){
+        String censoredWord = "";
         for(int i=1;i <word.length()+1;i++){
             censoredWord = censoredWord + "*";
         }
         return censoredWord;
+    }
 
+    public static String verify(char letter){
+        String verifiedWord = "";
+        for(int i=1;i<word.length()+1;i++){
+            if(word.charAt(i-1)==letter){
+                verifiedWord = verifiedWord + letter;
+                word.indexOf(letter, word.indexOf(letter)+1);
+            } else {
+                verifiedWord = verifiedWord + "*";
+            }   
+        }
+        return verifiedWord;
     }
     public static void main(String[] args) {
-        String test = "tennis";
-        System.out.println(censure(test));
+        System.out.println("You have 10 attempts to find : "+censure());
+        System.out.println(verify('n'));
     }
 }
