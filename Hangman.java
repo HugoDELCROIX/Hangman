a691093843d51ba737781b939ff4b27c9b221fb7
HugoDELCROIX <hugo.delcroix@hotmail.com>
2022-03-22 13:24:43 +0100

    censor method

--- Hangman.java
+++ Hangman.java
@@ -1,7 +1,20 @@
 public class Hangman {
-    protected static String word = "test";
+    protected static String word = "tennis";
+    protected boolean win = false;
 
+    public static String censure(String censoredWord){
+        int count = 0;
+        censoredWord = "";
+        for(int i=1;i <word.length()+1;i++){
+            count = i;
+            censoredWord = censoredWord + "*";
+            System.out.println(count);
+        }
+        return censoredWord;
+
+    }
     public static void main(String[] args) {
-        System.out.println(word);
+        String test = "tennis";
+        System.out.println(censure(test));
     }
 }
